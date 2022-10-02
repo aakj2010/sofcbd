2a5966bfe11aefb9fabe7369e464bfb1bdd723d8
aakj2010 <aakj2010@gmail.com>
2022-10-02 12:03:05 +0530

    net

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const DB = "sofc"
 app.use(express.json())
 app.use(cors({
     // origin: 'http://localhost:3000'
-    origin:'https://superb-sprite-48c5a0.netlify.app'
+    origin: 'https://superb-sprite-48c5a0.netlify.app'
 }))
 
 
